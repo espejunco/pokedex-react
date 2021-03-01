715323ceb3817f11b77e3fe01d58f61ded711c5a
espe <esperanza.del.junco@adaits.es>
2021-03-01 15:33:38 +0100

    containers and views

--- src/containers/Root/index.tsx
+++ src/containers/Root/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Home from "containers/Home";
+import Pokemon from 'containers/Pokemon';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path = "/" component = {Home}/>
+          <Route exact path = "/Pokemon/:name" component = {Pokemon}/>
+        <Redirect to = "/"/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
