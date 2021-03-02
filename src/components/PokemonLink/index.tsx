ede8a3ecead421add2a4ef0cfcfeb7ec1e621efd
espe <esperanza.del.junco@adaits.es>
2021-03-02 19:30:41 +0100

    Finished components

--- src/components/PokemonLink/index.tsx
+++ src/components/PokemonLink/index.tsx
@@ -1,18 +1,28 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import 'components/PokemonLink/styles.css';
 
 type Props = {
     name : string;
     image : string;
-}
+    lastVisited: boolean;
+};
 
-const PokemonLink: FC<Props> = ({name, image}) => {
+const PokemonLink: FC<Props> = ({name, image, lastVisited}) => {
+  if (lastVisited) {
+    return (
+      <NavLink to={`/pokemon/${name}`} className="link_root">
+        <img src={image} alt="link_image" className="link_image_visited" />
+      </NavLink>
+      )
+    }
+
+
   return (
-    <Link to={`/pokemon/${name}`}>
-    <img className="img_link" src={ image } alt="img_link" />
-    </Link>
+    <NavLink className="link_root" to={`/pokemon/${name}`}>
+      <img className="link_image" src={ image } alt="img_link" />
+    </NavLink>
 
   );
 };
